c9b092c7a1bd0ffbe11e265a57db4be0551fa824
ruivalente99 <rui.valente99@gmail.com>
2021-08-13 16:36:37 +0100

    HEL-5 Complete the crash course

--- redux-crashcourse/redux-crashcourse/src/App.js
+++ redux-crashcourse/redux-crashcourse/src/App.js
@@ -1,14 +1,18 @@
 import logo from "./logo.svg";
 import "./App.css";
+import {Provider} from 'react-redux';
 import Posts from "./components/Posts";
 import Postform from "./components/Postform";
+import store from "./store"
 function App() {
   return (
+    <Provider store={store}>
     <div className="App">
       <Postform />
       <hr/>
       <Posts />
     </div>
+    </Provider>
   );
 }
 
--- redux-crashcourse/redux-crashcourse/src/components/Postform.js
+++ redux-crashcourse/redux-crashcourse/src/components/Postform.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
-
+import {connect} from "react-redux";
+import PropTypes from 'prop-types';
+import {createPost} from '../actions/postActions'
 class Postform extends Component {
   constructor(props) {
     super(props);
@@ -19,15 +21,7 @@ class Postform extends Component {
           title: this.state.title,
           body: this.state.body
       }
-      fetch("https://jsonplaceholder.typicode.com/posts",{
-          method: "POST",
-          headers: {
-              'content-type': 'application/json'
-          },
-          body: JSON.stringify(post)
-      })
-      .then(res => res.json())
-      .then(data => console.log(data))
+      this.props.createPost(post);
   }
   render() {
     return (
@@ -61,5 +55,7 @@ class Postform extends Component {
     );
   }
 }
-
-export default Postform;
+Postform.propTypes = {
+  createPost: PropTypes.func.isRequired
+};
+export default connect(null,{createPost})(Postform);
