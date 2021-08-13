5037a182c8efe5b347c8ae87ae5463a1d41691ea
ruivalente99 <rui.valente99@gmail.com>
2021-08-13 11:34:31 +0100

    HEL-5 Redux Crash course

---
+++ redux-crashcourse/redux-crashcourse/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Posts from "./components/Posts";
+import Postform from "./components/Postform";
+function App() {
+  return (
+    <div className="App">
+      <Postform />
+      <hr/>
+      <Posts />
+    </div>
+  );
+}
+
+export default App;
